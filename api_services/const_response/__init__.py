b9ec603c971fbfb12a555b01e14dc509513ee3d9
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-08-29 22:00:33 +0100

    const response

--- api_services/const_response/__init__.py
+++ api_services/const_response/__init__.py
@@ -1,14 +1,19 @@
-# create a django function that returns http response
 from rest_framework.response import Response
 
 
 def return_response(
-    message: str, status_message: str, status: int = 200, data=None
+    status_message: str, status: int, message: str, data=None, **kwargs
 ) -> Response:
     res = {
-        "message": message,
         "status": status_message,
+        "message": message,
     }
+
     if data is not None:
+        # If data is explicitly passed, put it under "data"
         res["data"] = data
+    elif kwargs:
+        # If kwargs are passed, unpack into root
+        res.update(kwargs)
+
     return Response(res, status=status)
