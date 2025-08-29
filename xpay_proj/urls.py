102bba393f36e8ec38d80432f2a664b838ce97a2
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-08-29 22:03:30 +0100

    Update urls.py

--- xpay_proj/urls.py
+++ xpay_proj/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
+from api_services.custom_exceptions import CustomException
 
 BASE_PREFIX = "api/v1"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(f"{BASE_PREFIX}/ping/", include("apis.ping.urls")),
+    path(f"{BASE_PREFIX}/users/", include("apis.users.urls")),
+    path(f"{BASE_PREFIX}/auth/", include("apis.authentication.urls")),
 ]
+
+handler404 = CustomException.custom_404_view
+handler500 = CustomException.custom_500_view
