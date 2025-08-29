6c464460bb1449f0ac16ddbf15ce250419cfb62f
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-08-29 22:02:36 +0100

    Update views.py

--- apis/ping/views.py
+++ apis/ping/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from api_services.const_response import return_response
-from api_services.status_messages import StatusResponse as StatusRes
+from api_services.status_messages import StatusResponse as Res
 from rest_framework.decorators import api_view
 from rest_framework import status
 
@@ -8,4 +8,4 @@ from rest_framework import status
 # Create your views here.
 @api_view(["GET"])
 def ping(request):
-    return return_response("Pong", StatusRes.SUCCESS, status.HTTP_200_OK)
+    return return_response(Res.SUCCESS, status.HTTP_200_OK, "Pong")
